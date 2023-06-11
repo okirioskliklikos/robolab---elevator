211b62a601ac61e1e351e2f47eeff3d33081a3f5
okirioskliklikos <103096515+okirioskliklikos@users.noreply.github.com>
2023-06-11 16:43:41 +0300

    simplify code and remove unneeded variables

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(REQUESTED_FLOOR)
 })
 function stop_servos () {
-    SERVO_ISTURNING = false
+    SERVO_IS_TURNING = false
     DIRECTION = 0
     wuKong.stopMotor(wuKong.MotorList.M1)
     wuKong.stopMotor(wuKong.MotorList.M2)
@@ -18,21 +18,20 @@ function stop_servos () {
 input.onButtonPressed(Button.B, function () {
     if (REQUESTED_FLOOR > CURRENT_FLOOR) {
         DIRECTION = 1
-        SERVO_ISTURNING = true
+        SERVO_IS_TURNING = true
         wuKong.setMotorSpeed(wuKong.MotorList.M1, SERVO_SPEED)
         basic.showNumber(CURRENT_FLOOR)
     } else if (REQUESTED_FLOOR < CURRENT_FLOOR) {
         DIRECTION = 2
-        SERVO_ISTURNING = true
+        SERVO_IS_TURNING = true
         wuKong.setMotorSpeed(wuKong.MotorList.M1, 0 - SERVO_SPEED)
         basic.showNumber(CURRENT_FLOOR)
     }
 })
-let STOP_POINT = 0
 let BETWEEN_FLOORS = false
 let SENSOR_DISTANCE = 0
 let DIRECTION = 0
-let SERVO_ISTURNING = false
+let SERVO_IS_TURNING = false
 let REQUESTED_FLOOR = 0
 let CURRENT_FLOOR = 0
 let SERVO_SPEED = 0
@@ -43,11 +42,7 @@ stop_servos()
 basic.showNumber(CURRENT_FLOOR)
 basic.forever(function () {
     SENSOR_DISTANCE = grove.measureInCentimeters(DigitalPin.P8)
-    if (!(SERVO_ISTURNING)) {
-        if (BETWEEN_FLOORS) {
-            led_strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        }
-    } else {
+    if (SERVO_IS_TURNING) {
         if (SENSOR_DISTANCE >= 30) {
             if (!(BETWEEN_FLOORS)) {
                 BETWEEN_FLOORS = true
@@ -58,25 +53,19 @@ basic.forever(function () {
             if (BETWEEN_FLOORS) {
                 BETWEEN_FLOORS = false
                 if (DIRECTION == 1) {
-                    STOP_POINT += 1
+                    CURRENT_FLOOR += 1
                 } else if (DIRECTION == 2) {
-                    STOP_POINT += -1
+                    CURRENT_FLOOR += -1
                 }
-                if (STOP_POINT == 0 || STOP_POINT == 1 || STOP_POINT == 2) {
-                    CURRENT_FLOOR = STOP_POINT
-                    // Showing microbit leds delays reactions
-                    if (CURRENT_FLOOR == REQUESTED_FLOOR) {
-                        stop_servos()
-                    }
-                    basic.showNumber(CURRENT_FLOOR)
-                    if (CURRENT_FLOOR == REQUESTED_FLOOR) {
-                        led_strip.showColor(neopixel.colors(NeoPixelColors.Green))
-                        basic.pause(500)
-                        led_strip.showColor(neopixel.colors(NeoPixelColors.Black))
-                    }
-                } else {
-                    // is needed to give the time to really switch to next area
-                    basic.pause(200)
+                // Showing microbit leds delays reactions
+                if (CURRENT_FLOOR == REQUESTED_FLOOR) {
+                    stop_servos()
+                }
+                basic.showNumber(CURRENT_FLOOR)
+                if (CURRENT_FLOOR == REQUESTED_FLOOR) {
+                    led_strip.showColor(neopixel.colors(NeoPixelColors.Green))
+                    basic.pause(500)
+                    led_strip.showColor(neopixel.colors(NeoPixelColors.Black))
                 }
             }
         }
